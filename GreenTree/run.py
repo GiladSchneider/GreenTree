d512807be3f45a07bf05027a5c173a8e38ec93c9
GiladSchneider <giladschneider2@gmail.com>
2022-07-25 09:41:32 -0400

    file restructuring

--- GreenTree/run.py
+++ GreenTree/run.py
@@ -1,10 +1,5 @@
 from flask import Flask, render_template
 from config import app
-from views import strains, products, product_attributes, attributes
-
-@app.route('/')
-@app.route('/home')
-def index():
-    return render_template('index.html')
+from views import index, strains, products, product_attributes, attributes
 
 app.run(debug=True)
